0d71bc1f8cafe9adfb648cb3bce3aa39045c5842
Alex Hernandez <alexandre_2420@outlook.com>
2021-03-18 16:24:20 -0500

    configuracion del proyecto

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path'); //path esta dentro de node
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  mode: 'development',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'public/manifest.json', to: '' },
+    //     { from: 'public/service-worker.js', to: '' },
+    //     { from: 'public/icon.png', to: 'assets' },
+    //   ],
+    // }),
+  ],
+};
